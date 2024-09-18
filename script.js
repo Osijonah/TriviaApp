68ec20b28f5c370af388cf7b4989cf65eb39d3f8
Osijonah <osijonah@gmail.com>
2024-09-18 22:05:30 +0100

    achieved transition from homepage to quiz page with first question loading successfully

--- script.js
+++ script.js
@@ -1,47 +1,99 @@
-function changeContent (page) {
-    let contentSpace =  document.querySelector(`main`);
-    switch (page) {
-        case `home`:
-            contentSpace.innerHTML = `        <figure>
-            <img src="quiz.svg" alt="Quiz SVG">
-        </figure>
-        <div class="text">
-            <p class="header-size">Welcome to the Trivia Challenge</p>
-            <p>You will be presented with 10 True or False questions</p>
-            <p>Can you score 100%?</p>
-            <div>
-                <a href="#" onclick="changeContent('quiz')" class="btn">BEGIN</a>
-            </div>
-        </div>`;
-            break;
-        case `quiz`:
-            contentSpace.innerHTML = `<div class="text question-text"><div>
-            <p class="question-title">question-title</p>
-            <p class="question">question</p>
-        </div>
-        <div class="answer-btns">
-            <div class="true-btn">✔True</div>
-            <div class="false-btn">❌False</div>
-        </div>
-        <div>
-            <a href="#" onclick="changeContent('result')" class="btn">CONTINUE</a> 
-        </div></div>`;
-        break;
-        case `result`:
-            contentSpace.innerHTML = `<div class="text"><div class="score-div">
-            <p class="score-p">You Scored </p>
-        </div>
-        <div class="result">
-            <div>
-                <p>Question</p>
-                <p>Answer</p>
-            </div>
-            <div></div>
-        </div>
-        <div>
-            <a href="#" onclick="changeContent('home')" class="btn">TRY AGAIN</a> 
-        </div></div>`
-        default:
-            break;
-    }
+async function startQuiz() {
+    const requestURL =
+      "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean";
+    const request = new Request(requestURL);
+  
+    const response = await fetch(request);
+    const quizQuestions = await response.json();
+  
+    populateQuiz(quizQuestions)
 }
+
+
+
+const questionTitle = document.querySelector(`.question-title`);
+const question = document.querySelector(`.question`);
+
+
+
+const contentSpace =  document.querySelector(`main`);
+const textDiv = document.querySelector(`.text`);
+const figure  = document.querySelector(`figure`);
+
+document.querySelector(`.begin-btn`).addEventListener(`click`, startQuiz);
+
+  function populateQuiz (obj) {
+    console.log(obj);
+        const quizDiv = document.createElement(`div`);
+        const questionDiv = document.createElement(`div`);
+        const questionTitleParagraph = document.createElement(`p`);
+        const questionParagraph = document.createElement(`p`);
+        const btnDiv = document.createElement(`div`);
+        const trueBtn = document.createElement(`div`);
+        const falseBtn = document.createElement(`div`);
+        const contBtnDiv = document.createElement(`div`);
+        const contBtn = document.createElement(`a`);
+        
+        
+        
+        quizDiv.classList.add(`text`, `question-text`);
+        questionTitleParagraph.classList.add(`question-title`);
+        questionParagraph.classList.add(`question`);
+        btnDiv.classList.add(`answer-btns`);
+        trueBtn.classList.add(`true-btn`);
+        falseBtn.classList.add(`false-btn`);
+        contBtn.classList.add(`btn`);
+
+        questionTitleParagraph.textContent = obj.results[0].category;
+        questionParagraph.textContent = obj.results[0].question;
+        
+        trueBtn.textContent = `✔True`;
+        falseBtn.textContent = `❌False`;
+        contBtn.textContent = `CONTINUE`;
+        
+        
+        questionDiv.appendChild(questionTitleParagraph)
+        questionDiv.appendChild(questionParagraph)
+        btnDiv.appendChild(trueBtn);
+        btnDiv.appendChild(falseBtn);
+        contBtnDiv.appendChild(contBtn);
+        quizDiv.appendChild(questionDiv);
+        quizDiv.appendChild(btnDiv);
+        quizDiv.appendChild(contBtnDiv);
+
+        textDiv.style.display = `none`;
+        figure.classList.add(`no-display`);
+        contentSpace.appendChild(quizDiv);
+        
+
+
+        // <p class="question-title">1 of 10 (${questionTitle})</p>
+    
+      }
+    
+    // populateQuiz(quizQuestions);
+    
+// function changeContent (page) {
+    
+    
+    
+    // onclick="changeContent('quiz')"
+//     switch (page) {
+//              case `result`:
+//             contentSpace.innerHTML = `<div class="text"><div class="score-div">
+//             <p class="score-p">You Scored </p>
+//         </div>
+//         <div class="result">
+//             <div>
+//                 <p>Question</p>
+//                 <p>Answer</p>
+//             </div>
+//             <div></div>
+//         </div>
+//         <div>
+//             <a href="#" onclick="changeContent('home')" class="btn">TRY AGAIN</a> 
+//         </div></div>`
+//         default:
+//             break;
+//     }
+// }
